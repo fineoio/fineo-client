a224adfb5a373fe5bd7761f773f445bf642db0a3
Jesse Yates <jesse@fineo.io>
2016-09-29 16:05:16 -0700

    Making Empty model ignore properties

--- java/models/common-models/src/main/java/io/fineo/client/model/Empty.java
+++ java/models/common-models/src/main/java/io/fineo/client/model/Empty.java
@@ -1,7 +1,10 @@
 package io.fineo.client.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  *
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Empty {
 }
