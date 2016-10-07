c2f1687d608b1eb07e1688016a84f0a5ef2510ec
Jesse Yates <jesse@fineo.io>
2016-10-07 11:52:55 -0700

    Making file required for batch upload

--- java/tools/src/main/java/io/fineo/client/tools/option/BatchUploadOption.java
+++ java/tools/src/main/java/io/fineo/client/tools/option/BatchUploadOption.java
@@ -5,10 +5,11 @@ import com.beust.jcommander.Parameter;
 
 public class BatchUploadOption {
 
-  @Parameter(names = {"-f", "--file"}, description = "Full path to the file to upload")
+  @Parameter(names = {"-f", "--file"}, description = "Full path to the file to upload",
+             required = true)
   public String file;
 
-  @Parameter(names ={"-c", "--check-status"},
+  @Parameter(names = {"-c", "--check-status"},
              description = "If we should check the state of the file")
   public boolean check = false;
 }
